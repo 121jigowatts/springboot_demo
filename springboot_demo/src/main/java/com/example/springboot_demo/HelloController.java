64a7ca195bcdbbb7ecc3cbf7fd94aad3305b6876
soil <sh.yoshida@live.jp>
2018-09-08 01:28:07 +0900

    edit style

--- springboot_demo/src/main/java/com/example/springboot_demo/HelloController.java
+++ springboot_demo/src/main/java/com/example/springboot_demo/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.springboot_demo;
 
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,5 +33,17 @@ public class HelloController {
 	public ModelAndView form(@ModelAttribute("formModel") User user, ModelAndView model) {
 		repository.saveAndFlush(user);
 		return new ModelAndView("redirect:/");
+	}
+
+	@PostConstruct
+	public void initalData() {
+		ArrayList<User> data = new ArrayList<User>();
+		data.add(new User("Alice", "Alice@example.com", 16));
+		data.add(new User("Emma","Emma@example.com",19));
+		data.add(new User("Olivia","Olivia@example.com",17));
+
+		for (User user : data) {
+			repository.saveAndFlush(user);
+		}
 	}
 }
