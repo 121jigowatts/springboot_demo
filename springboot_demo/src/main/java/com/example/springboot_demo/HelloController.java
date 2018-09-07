d653ce8f5bf7b6904b151be3ac59a2d79fb76b49
soil <sh.yoshida@live.jp>
2018-09-07 23:29:17 +0900

    first JPA

--- springboot_demo/src/main/java/com/example/springboot_demo/HelloController.java
+++ springboot_demo/src/main/java/com/example/springboot_demo/HelloController.java
@@ -1,12 +1,33 @@
 package com.example.springboot_demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+import com.example.springboot_demo.repositories.UserRepository;
+
+@Controller
 public class HelloController {
-	@RequestMapping("/")
-	public String index() {
-		return "Hello World!";
+	@Autowired
+	UserRepository repository;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(@ModelAttribute("formModel") User user, ModelAndView model) {
+		model.setViewName("index");
+		model.addObject("message", "Hello World!");
+		Iterable<User> list = repository.findAll();
+		model.addObject("users", list);
+		return model;
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@Transactional(readOnly = false)
+	public ModelAndView form(@ModelAttribute("formModel") User user, ModelAndView model) {
+		repository.saveAndFlush(user);
+		return new ModelAndView("redirect:/");
 	}
 }
