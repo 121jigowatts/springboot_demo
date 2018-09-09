4f1b486051b9d290434ff5b6327045d35a9c6d4c
soil <sh.yoshida@live.jp>
2018-09-09 09:41:09 +0900

    add edit page

--- springboot_demo/src/main/java/com/example/springboot_demo/HelloController.java
+++ springboot_demo/src/main/java/com/example/springboot_demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.springboot_demo;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,7 +22,7 @@ public class HelloController {
 	UserRepository repository;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView index(@ModelAttribute("formModel") User user, ModelAndView model) {
+	public ModelAndView index(@ModelAttribute("formModel") User userEntity, ModelAndView model) {
 		model.setViewName("index");
 		model.addObject("message", "Hello World!");
 		Iterable<User> list = repository.findAll();
@@ -30,8 +32,23 @@ public class HelloController {
 
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	@Transactional(readOnly = false)
-	public ModelAndView form(@ModelAttribute("formModel") User user, ModelAndView model) {
-		repository.saveAndFlush(user);
+	public ModelAndView form(@ModelAttribute("formModel") User userEntity, ModelAndView model) {
+		repository.saveAndFlush(userEntity);
+		return new ModelAndView("redirect:/");
+	}
+
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public ModelAndView edit(@ModelAttribute User userEntity, @PathVariable int id, ModelAndView model) {
+		model.setViewName("edit");
+		Optional<User> user = repository.findById((long) id);
+		model.addObject("formModel", user.get());
+		return model;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST)
+	@Transactional(readOnly = false)
+	public ModelAndView update(@ModelAttribute User userEntity, ModelAndView model) {
+		repository.saveAndFlush(userEntity);
 		return new ModelAndView("redirect:/");
 	}
 
@@ -39,8 +56,8 @@ public class HelloController {
 	public void initalData() {
 		ArrayList<User> data = new ArrayList<User>();
 		data.add(new User("Alice", "Alice@example.com", 16));
-		data.add(new User("Emma","Emma@example.com",19));
-		data.add(new User("Olivia","Olivia@example.com",17));
+		data.add(new User("Emma", "Emma@example.com", 19));
+		data.add(new User("Olivia", "Olivia@example.com", 17));
 
 		for (User user : data) {
 			repository.saveAndFlush(user);
--- springboot_demo/src/main/java/com/example/springboot_demo/repositories/UserRepository.java
+++ springboot_demo/src/main/java/com/example/springboot_demo/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.springboot_demo.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.springboot_demo.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	public Optional<User> findById(long id);
 }
