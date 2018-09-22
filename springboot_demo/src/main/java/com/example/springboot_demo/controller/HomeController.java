95a1ec4cd700666c11183f2f51e9c15c2aa986ef
soil <sh.yoshida@live.jp>
2018-09-23 08:25:39 +0900

    add HomeController

---
+++ springboot_demo/src/main/java/com/example/springboot_demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.springboot_demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/home/")
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("/home/index");
+		return mav;
+	}
+}
