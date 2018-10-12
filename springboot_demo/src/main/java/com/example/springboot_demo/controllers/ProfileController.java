033b93f68d69624cffde200c786e75bc443da1db
soil <sh.yoshida@live.jp>
2018-10-12 23:39:19 +0900

    add ProfileController

---
+++ springboot_demo/src/main/java/com/example/springboot_demo/controllers/ProfileController.java
@@ -0,0 +1,29 @@
+package com.example.springboot_demo.controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.springboot_demo.domain.User;
+import com.example.springboot_demo.services.AccountService;
+
+@RestController
+public class ProfileController {
+
+	@Autowired
+	private AccountService service;
+
+	@RequestMapping(value = "/home/profile", method = RequestMethod.GET)
+	public ModelAndView changeProfile(@RequestParam("id") String id, ModelAndView mav) {
+		mav.setViewName("/home/profile");
+		Optional<User> user = service.findUserById(id);
+		mav.addObject(user.get());
+		return mav;
+	}
+
+}
