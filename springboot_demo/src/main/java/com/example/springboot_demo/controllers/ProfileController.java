333e1c10cc75fc51bc201aad850373e23931a286
soil <sh.yoshida@live.jp>
2018-10-15 23:56:32 +0900

    add profile

--- springboot_demo/src/main/java/com/example/springboot_demo/controllers/ProfileController.java
+++ springboot_demo/src/main/java/com/example/springboot_demo/controllers/ProfileController.java
@@ -3,6 +3,8 @@ package com.example.springboot_demo.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,8 +24,17 @@ public class ProfileController {
 	public ModelAndView changeProfile(@RequestParam("id") String id, ModelAndView mav) {
 		mav.setViewName("/home/profile");
 		Optional<User> user = service.findUserById(id);
-		mav.addObject(user.get());
+		mav.addObject("formModel", user.get());
 		return mav;
+	}
+	
+	@RequestMapping(value = "/home/profile", method = RequestMethod.POST)
+	@Transactional(readOnly = false)
+	public ModelAndView update(@ModelAttribute User user, ModelAndView model) {
+		
+		service.saveUser(user);
+		
+		return new ModelAndView("redirect:/home/profile");
 	}
 
 }
